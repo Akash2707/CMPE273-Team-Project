d07ab0c4819ad6cca22aabc5d3c8d04793f3e3f5
Aakash <akashthakkar1997@gmail.com>
2018-11-20 01:01:15 -0800

    designed navigation bar

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import {Route} from 'react-router-dom';
 import ApplicantLogin from './Login/applicantLogin';
+import Navbar from './Navbar/Navbar'
 
 class Main extends Component {
     render(){
         return(
             <div>
                 {/*Render Different Component based on Route*/}
+                <Route path= "/" component = {Navbar}/>
                 <Route path="/applicantlogin" component={ApplicantLogin}/>
             </div>
         )
